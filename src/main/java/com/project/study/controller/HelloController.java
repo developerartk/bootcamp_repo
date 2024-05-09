f7bc9d0160a3cc01ba0e384b4982a1292be20344
Arhur Kulikovich <artur.kulikovits@wenture.io>
2024-05-09 08:05:05 +0300

    Even Odd numbers

--- src/main/java/com/project/study/controller/HelloController.java
+++ src/main/java/com/project/study/controller/HelloController.java
@@ -2,21 +2,42 @@ package com.project.study.controller;
 
 
 import com.project.study.dto.ResponseDto;
+import com.project.study.model.NumberSelector;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
-    private ResponseDto responseDto;
 
-    public HelloController(ResponseDto responseDto) {
-        this.responseDto = responseDto;
+    @PostMapping("/hello")
+    public ResponseDto getHelloWorld() {
+        return new ResponseDto();
     }
 
-    @GetMapping("/hello")
-    public ResponseDto getHelloWorld() {
+    @PostMapping("/helloNew")
+    public ResponseDto getHello(@RequestBody String name) {
+        ResponseDto responseDto = new ResponseDto(name);
+        System.out.println("Name: " + name);
         return responseDto;
     }
 
+    @GetMapping("/getOddNumbers")
+    public List<Integer> getOddNumbers(@RequestBody List<Integer> listOfNumbers){
+        NumberSelector numberSelector = NumberSelector.getInstance();
+        return numberSelector.getOdd(listOfNumbers);
+    }
+
+    @GetMapping("/getEvenNumbers")
+    public List<Integer> getEvenNumbers(@RequestBody List<Integer> listOfNumbers){
+        NumberSelector numberSelector = NumberSelector.getInstance();
+        return numberSelector.getEven(listOfNumbers);
+    }
+
 }
+
+
