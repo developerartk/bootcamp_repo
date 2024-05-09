dced74a7159075aebbedf45cc47b10d11275c14f
Arhur Kulikovich <artur.kulikovits@wenture.io>
2024-05-09 08:05:35 +0300

    Even Odd numbers

--- src/main/java/com/project/study/controller/HelloController.java
+++ src/main/java/com/project/study/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.project.study.controller;
 
 import com.project.study.dto.ResponseDto;
 import com.project.study.model.NumberSelector;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
